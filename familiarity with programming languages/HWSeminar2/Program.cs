44ae5ba5166381aeaefb83c2c23a71f4c0395d34
MihaLis333 <m190333@ya.ru>
2023-03-01 14:31:17 +0300

    initial commit

---
+++ familiarity with programming languages/HWSeminar2/Program.cs
@@ -0,0 +1,73 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+/*Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+int doz = num / 10 % 10;
+
+if(num > 99 || num < 999)
+{
+    Console.WriteLine(doz);
+}
+if (num < 99 || num > 999)
+{
+    Console.WriteLine("Это не трехзначное число!");
+} */
+
+
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+ что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+/*int thNum(int num)
+{
+    if (num > 99)
+    {
+        int ed = num % 10;
+        Console.WriteLine(ed);
+    }
+    else 
+    {
+        Console.WriteLine("Введите трехзначное число!");
+    }   
+    return num;
+}
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+thNum(num); */
+
+/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+ и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+/* Console.WriteLine("введите цифру дня недели");
+int day = Convert.ToInt32 (Console.ReadLine());
+int n = day;
+int weekend1 = 6;
+int weekend2 = 7;
+
+if (day == weekend1 || day == weekend2)
+{
+    Console.WriteLine("Выходной день");
+}
+if (day > 7)
+{
+    Console.WriteLine("Такого дня недели - НЕТ!");
+}
+if (day < 6)
+{
+    Console.WriteLine("Будний день");
+} */
+
+
+
+
+
---
+++ familiarity with programming languages/HWSeminar3/Program.cs
@@ -0,0 +1,68 @@
+﻿/*Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт
+таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+/* Console.Write("Input number: ");
+double num = int.Parse(Console.ReadLine());
+
+for (double i = 1; i <= num; i++)
+{
+    double quatro = i * i * i;
+    Console.WriteLine(quatro);
+} */
+
+/*Задача 21. Напишите программу, которая принимает на вход координаты двух 
+точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+
+/*Console.Write("Input coordinat point Xa: ");
+double xa = int.Parse(Console.ReadLine());
+Console.Write("Input coordinat point Ya: ");
+double ya = int.Parse(Console.ReadLine());
+Console.Write("Input coordinat point Za: "); 
+double za = int.Parse(Console.ReadLine());
+
+Console.Write("Input coordinat point Xb: ");
+double xb = int.Parse(Console.ReadLine());
+Console.Write("Input coordinat point Yb: "); 
+double yb = int.Parse(Console.ReadLine());
+Console.Write("Input coordinat point Zb: "); 
+double zb = int.Parse(Console.ReadLine());
+
+double ac = Math.Pow(xb - xa, 2); 
+double bc = Math.Pow(yb - ya, 2);
+double zz = Math.Pow(zb - za, 2); 
+
+//double ac2 = Math.Pow(ac, 2); 
+//double bc2 = Math.Pow(bc, 2);
+Console.WriteLine($"{Math.Sqrt(ac + bc + zz)}"); */
+
+
+/* Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+Console.Write("Input five-digit number: ");
+int num = int.Parse(Console.ReadLine());
+
+
+int num4 = num % 10;
+int num3 = num % 10 / 10;
+int num2 = num / 100 % 10; // в этих переменных я выделил последние 3 цифры с конца
+int revers =  // пока не пойму что сюда писать
+
+
+if (num == revers)
+{
+    Console.WriteLine(num4);
+}
+else
+{
+   Console.WriteLine(" Not Palindrom");
+}
+
+
