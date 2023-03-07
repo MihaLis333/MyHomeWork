6e009f4e3f7ead45f4a875417581c995cae19b5f
MihaLis333 <m190333@ya.ru>
2023-03-07 15:17:56 +0300

    initial commit

---
+++ familiarity with programming languages/HWSeminar5/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными 
+трёхзначными числами. Напишите программу, которая покажет 
+количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int[] FillArray (int Array)
+{
+    int[] res = new int[Array];
+    for (int i = 0; i < Array; i++)
+    {
+        res[i] = new Random().Next(100, 999);
+        //Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+void PrintArray(int[] inArray)
+{
+    for(int ind = 0; ind < inArray.Length; ind++)
+    {
+        Console.Write($"{inArray[ind]} ");
+    }
+}
+void EvenNumbers(int[] inArray)
+{
+    for(int index = 0; index < inArray.Length / 2; index++)
+    {
+            Console.Write($"{inArray[index]} "); // выводит на оборот те что не деляться на 2!
+    }
+}
+int[] iArray = FillArray(4);
+PrintArray(iArray);
+Console.WriteLine();
+EvenNumbers(iArray);
