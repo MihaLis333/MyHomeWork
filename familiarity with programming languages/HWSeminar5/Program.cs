c5e3242d833ab22d03b2aef82e8596adaf9fdc36
MihaLis333 <m190333@ya.ru>
2023-03-11 22:08:45 +0300

    added task two

--- familiarity with programming languages/HWSeminar5/Program.cs
+++ familiarity with programming languages/HWSeminar5/Program.cs
@@ -3,7 +3,7 @@
 количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2 */
 
-int[] FillArray (int Array)
+/*int[] FillArray (int Array)
 {
     int[] res = new int[Array];
     for (int i = 0; i < Array; i++)
@@ -28,11 +28,67 @@ void EvenNumbers(int[] inArray)
         if(inArray[index] % 2 == 0)
         {
             count++;
-        }
-        Console.Write($"{count} ");
+        }   
     }
+    Console.Write($"{count} ");
 }
 int[] iArray = FillArray(4);
 PrintArray(iArray);
 Console.WriteLine();
-EvenNumbers(iArray);
+EvenNumbers(iArray); */
+
+
+/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+/*int[] FillArray (int Array)
+{
+    int[] res = new int[Array];
+    for (int i = 0; i < Array; i++)
+    {
+        res[i] = new Random().Next(1, 10);
+        //Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+void PrintArray (int[] pArray)
+{
+    for(int pi = 0; pi < pArray.Length; pi++)
+    {
+        Console.Write($"{pArray[pi]} ");
+    }
+}
+
+void SumElements(int[] sArray)
+{
+    int numodd = 0;
+    int numeven = 0;
+    foreach (int element in sArray)
+    {
+        if (element % 2 != 0)
+        {
+            numodd += element;
+        }
+        else
+        {
+            numeven += element;
+        }
+    }
+    Console.WriteLine($"{numodd}");
+}
+
+int[] array = FillArray(4);
+PrintArray(array);
+Console.WriteLine();
+SumElements(array); */
+
+
+/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между 
+максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76 
+*/
+
+
