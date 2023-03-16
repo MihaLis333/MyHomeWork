8950881ec7f4c7d13b52f0be36128f1fb3ac1e64
MihaLis333 <m190333@ya.ru>
2023-03-16 15:54:10 +0300

    fuck

---
+++ familiarity with programming languages/HWSeminar6/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int[] Main(string[] args)
+{
+    Console.Write("Input element array: ");
+
+    int elementCount = int.Parse(Console.ReadLine());
+
+    int[] MArray = new int [elementCount];
+
+    for (int i =0; i < MArray.Length; i++)
+    {
+        Console.Write($"\nInput element array in index {i}:\t ");
+        MArray[i] = int.Parse(Console.ReadLine());
+    }
+
+    for(int i = 0; i < MArray.Length; i++)
+    {
+        
+        Console.Write($"\n{MArray[i]}");
+    }
+    return MArray;
+}
+
+void OutNum(string num)
+{ 
+    int n = Convert.ToInt32(num);
+    int[] OArray = new int[n];
+
+    for(int i =0; i < OArray.Length; i++)
+    {
+        if(OArray[i] > 0)
+        {
+            Console.Write(OArray[i]);
+        }
+    }
+} 
+Main(args);
+OutNum(args);
